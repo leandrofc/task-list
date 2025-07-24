3aaf399792b0e9accf68b2d2dd89e7878040269f
Leandro Fonseca <leandromkt@outlook.com>
2025-07-24 13:18:29 -0300

    feat: implement Footer Component ui

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import { Button } from "../Button";
+
+const Footer = () => {
+    return (
+        <div className="flex flex-col gap-6">
+            <Button
+                text="Add new task"
+                onClick={() => {}}
+                variant="primary"
+            />
+        </div>
+
+    )
+}
+
+export default Footer;
