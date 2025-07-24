bd98598f918ea5ab9d6cb23f4851a76b73c58e50
Leandro Fonseca <leandromkt@outlook.com>
2025-07-24 20:25:24 -0300

    feat: adjust taskList component to pass id and data for TaskItem component

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,4 +1,5 @@
 import { useTaskList } from "../../context/TaskListContext";
+import { formatDateToMMDDYY } from "../../utils/formatDate";
 import TaskItem from "../TaskItem";
 
 const TaskList = () => {
@@ -13,15 +14,20 @@ const TaskList = () => {
     return (
         <div className="flex flex-1 flex-col gap-3 overflow-y-auto mt-6 mb-6 p-3 items-center bg-gray-100 w-full border border-dashed rounded-xl border-gray-300 md:max-w-[493px]">
             {
-                filteredTasks?.map(({id, title, createdAt, completedAt}, index) => (
-                    <TaskItem
-                        key={id}
-                        title={title}
-                        date={`${createdAt} - ${completedAt}`}
-                        isCompleted={!!completedAt}
-                        index={index}
-                    />
-                ))
+                filteredTasks?.map(({id, title, createdAt, completedAt}, index) => {
+                    
+                    const dataText = completedAt ? `${formatDateToMMDDYY(createdAt)} - ${formatDateToMMDDYY(completedAt)}` : formatDateToMMDDYY(createdAt);
+                    
+                    return(
+                        <TaskItem
+                            key={id}
+                            title={title}
+                            date={dataText}
+                            isCompleted={!!completedAt}
+                            index={index}
+                            id={id}
+                        />
+                )})
             }
         </div>
     )
