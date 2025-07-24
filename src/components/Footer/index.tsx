ef125f4d127013faeeb66da52a76cfbfa120c7b7
Leandro Fonseca <leandromkt@outlook.com>
2025-07-24 14:07:55 -0300

    feat: use Modal component on Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { Button } from "../Button";
+import Modal from "../Modal";
 
 const Footer = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
         <div className="flex flex-col gap-6">
             <Button
                 text="Add new task"
-                onClick={() => {}}
+                onClick={() => setIsModalOpen(true)}
                 variant="primary"
+            />
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
             />
         </div>
 
