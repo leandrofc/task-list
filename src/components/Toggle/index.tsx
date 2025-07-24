236d267302e785747631b7212dfa9bbaba67c527
Leandro Fonseca <leandromkt@outlook.com>
2025-07-24 20:23:54 -0300

    feat: change the status of a task in the toggle component

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,11 +1,25 @@
 import { Check } from "react-feather";
 import type ToggleProps from "./index.types";
+import { useTaskList } from "../../context/TaskListContext";
 
-const Toggle = ({ isSelected }: ToggleProps) => {
+const Toggle = ({ isSelected, id  }: ToggleProps) => {
+    const { setTasks } = useTaskList();
+
+    const toggleTaskCompletion = () => {
+        const dateNow = new Date().toISOString();
+
+        setTasks((prev) =>  prev.map((task) => (
+            task.id === id ? { 
+                ...task,
+                completedAt: (task.completedAt ? null : dateNow) 
+            } : task)
+        ));
+    }
+
     return (
         <button
-            onClick={() => {}}
             className={`flex items-center justify-center w-11 h-11 rounded-xl ${isSelected ? 'bg-green-100 border-none' : 'bg-white border border-dashed border-gray-300'}`}
+            onClick={toggleTaskCompletion}
         >
             <Check
                 className={`
--- src/components/Toggle/index.types.ts
+++ src/components/Toggle/index.types.ts
@@ -1,3 +1,4 @@
 export default interface ToggleProps {
     isSelected?: boolean;
+    id: string;
 }
