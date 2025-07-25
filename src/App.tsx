5c898acaf52fb9bd8458ba0222f525ac81e1405c
Leandro Fonseca <leandromkt@outlook.com>
2025-07-24 23:20:11 -0300

    feat: add fallback route for non existent pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home'
 
 const App = () => {
@@ -6,6 +6,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
